8bfe381fbf1d21864c586207e5e50df20a633f38
MilanMixa <stankovicmilan990@gmail.com>
2021-09-08 22:49:31 +0200

    added bootstrap to header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <h1>kREACTivni</h1>;
+  return <Header />;
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 const Header = () => {
   return (
-    <header>
+    <header className="pt-3 bg-secondary text-white">
       <div className="container d-flex justify-content-between">
         <h1>
-          <span>I</span>nteviews <span>R</span>eports
+          <span>I</span>nterviews <span>R</span>eports
         </h1>
         <nav>
           <ul>
-            <li className="list-unstyled ">
-              <i class="fas fa-users"></i>Candidates
+            <li className="list-unstyled border py-2 px-4">
+              <i className="fas fa-users fs-3 pe-2"></i>Candidates
             </li>
           </ul>
         </nav>
