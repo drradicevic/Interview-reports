1c6e1d5dae6da1f6c4023e07aed6734f87100bf6
drradicevic <drradicevic@gmail.com>
2021-09-09 19:06:12 +0200

    Form css modified

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -4,43 +4,25 @@ import "./LoginForm.css";
 
 
 export const LoginForm = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-
-    const getUserNameHandler = (event) => {
-        setUsername(event.target.value);
-    };
-
-    const passwordHandler = (event) => {
-        setPassword(event.target.value);
-    };
-
-    const resetButton = () => {
-        setUsername("");
-        setPassword("");
-    };
-
+    
     return (
         <div className="vh-100">
     <div className="d-flex justify-content-center align-items-center backgcol h-100">
         <div className="d-flex justify-content-center align-items-center bg-white bgshadow h-75 w-75 bradius">
-            <div className=" border border-dark bradius">
+            <div className=" border border-dark bradius formStyle">
                 <form className="px-4 py-3">
                     <div className="form-group">
-                        <label htmlFor="exampleDropdownFormEmail1">Email address</label>
                         <input
                         type="email"
-                        className="form-control fas"
+                        className="form-control fas inpstyle bradius"
                         id="exampleDropdownFormEmail1"
                         placeholder="&#xf0e0; email@example.com"
                         />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleDropdownFormPassword1">Password</label>
                         <input
                         type="password"
-                        className="form-control fas"
+                        className="form-control fas inpstyle bradius"
                         id="exampleDropdownFormPassword1"
                         placeholder="&#xf023; Password"
                         />
