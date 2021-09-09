9ab3606f3a3ee86aa94e25c3453963efe0765826
6urrp <sndrtasic@gmail.com>
2021-09-09 19:49:09 +0200

    Footer and header, font family added, dummy main component created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
-  return <Header />;
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
+import "./Header.css";
+
 const Header = () => {
   return (
-    <header className="pt-3 bg-secondary text-white">
+    <header className="pt-3 text-white">
       <div className="container d-flex justify-content-between">
         <h1>
           <span>I</span>nterviews <span>R</span>eports
         </h1>
         <nav>
           <ul>
-            <li className="list-unstyled border py-2 px-4">
+            <li className="list-unstyled py-2 px-4">
               <i className="fas fa-users fs-3 pe-2"></i>Candidates
             </li>
           </ul>
