3cd9d270bc05ce402cc9be42fe7669531a3ab0dc
6urrp <sndrtasic@gmail.com>
2021-09-11 11:03:09 +0200

    Reports table created with bootstrap

--- src/components/ReportsPage/ReportsPage.jsx
+++ src/components/ReportsPage/ReportsPage.jsx
@@ -2,12 +2,14 @@ import { Fragment } from "react/cjs/react.production.min";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import AboutCandidate from "./AboutCandidate/AboutCandidate";
+import Reports from "./ReportsPage/ReportsPage";
 
 const ReportsPage = () => {
   return (
     <Fragment>
       <Header />
       <AboutCandidate/>
+      <Reports />
       <Footer />
     </Fragment>
   );
