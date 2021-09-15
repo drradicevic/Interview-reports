5eb62dd59f562ab07c456a7e3367fb286d4e614e
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-09-15 18:15:20 +0200

    reports data from props passed to reports table

--- src/components/ReportsPage/Reports/Reports.jsx
+++ src/components/ReportsPage/Reports/Reports.jsx
@@ -1,49 +1,32 @@
 import "./Reports.css";
 
-const Reports = () => {
+const Reports = ({ reports }) => {
   return (
     <div className="my-5 mx-5">
       <table className="table">
         <thead>
           <tr>
             <th scope="col" rowSpan="2">
-              First
+              Company
             </th>
             <th scope="col" rowSpan="2">
-              Last
+              Interview Date
             </th>
-            <th scope="col" colSpan="3">
-              Handle
+            <th scope="col" rowSpan="2">
+              Status
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <i className="fab fa-google"> Google</i>
-            </td>
-            <td>Otto</td>
-            <td>@medo</td>
-          </tr>
-          <tr>
-            <td> <i className="fab fa-facebook-f"> Facebook</i>
-            </td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td><i className="fab fa-instagram"> Instagram</i>
-            </td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-          <tr>
-            <td>
-              <i className="fab fa-linkedin-in"> Linkedin</i>
-            </td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
+          {reports.map((report, index) => {
+            return (
+              <tr key={index}>
+                <td>{report.companyName}</td>
+                <td>{report.interviewDate}</td>
+                <td>{report.status}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
