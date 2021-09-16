d26250cec0a27d365c0d180ce31b31a38ea341e8
6urrp <sandratasic@outlook.com>
2021-09-16 18:34:59 +0200

    Search field logic implemented

--- src/components/Candidates/Candidates.jsx
+++ src/components/Candidates/Candidates.jsx
@@ -10,24 +10,39 @@ const avatar = "https://perfectczechwomen.com/wp-content/uploads/2019/04/New-Pro
     
 
 const Candidates = () => {
-
     const [candidates, setCandidates] = useState([]);
+    const [filteredCandidates, setfilteredCandidates] = useState([]);
+    const [inputValue, setInputValue] = useState("");
 
     useEffect(() => {
         const token = localStorage.getItem("token");
         getCandidatesAPI("http://localhost:3333/api/candidates", token)
-            .then(candidates => setCandidates(candidates));
+            .then(candidates => {
+                setCandidates(candidates)
+                setfilteredCandidates(candidates)});
     }, [])
+
+    const onTypingHandler = (e) => {
+        setInputValue(e.target.value);
+        const filtered = candidates.filter(candidate => candidate.name.toLowerCase().includes(e.target.value.toLowerCase()))
+        setfilteredCandidates(filtered);
+    }
+
 
     return (
         <div className="w-75 mx-auto mt-5 mb-5">
             <div className="d-flex justify-content-between">
               <h2>Candidates</h2>
-              <input type="search" />
+              <input
+                value={inputValue}
+                placeholder= "search..."
+                type="text"
+                onChange={onTypingHandler}
+                />
             </div>
             <div className="d-flex flex-wrap justify-content-between text-center mt-5">
             {
-            candidates.map((candidate, index) => 
+            filteredCandidates.map((candidate, index) => 
                 <Card candidateInfo={candidate} key={index} avatar={avatar} />
             )
             }
